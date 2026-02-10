52078874ce7900e14e17db6ab6ff51183fc40962
Zakaria Mouhid <zakariamouhid@gmail.com>
2026-02-10 04:43:47 +0100

    Add Django REST framework support: include djangorestframework in Pipfile, register Menu and Booking models in admin, create UserViewSet for user management, and update URL routing for API access.

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    def __str__(self):
+        return f'{self.title} (${self.price})'
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+    def __str__(self):
+        return f'Booking for {self.name} on {self.booking_date.strftime("%Y-%m-%d %H:%M")} for {self.no_of_guests} guest(s)'
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework import viewsets
 
 # Create your views here.
 def sayHello(request):
@@ -7,3 +10,7 @@ def sayHello(request):
 
 def index(request):
     return render(request, 'index.html', {})
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
