7412bdbc91db4fea0677fb1d9b72b9359c9c05eb
Zakaria Mouhid <zakariamouhid@gmail.com>
2026-02-10 04:28:22 +0100

    Refactor URL routing and add index view: replace sayHello with index in urlpatterns and implement index view to render index.html.

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin 
 from django.urls import path 
-from .views import sayHello 
+from . import views 
+
   
 urlpatterns = [ 
-    path('', sayHello, name='sayHello'), 
+    # path('', views.sayHello, name='sayHello'),
+    path('', views.index, name='home'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def sayHello(request):
  return HttpResponse('Hello World', {'content-type': 'plain/text'})
+
+def index(request):
+    return render(request, 'index.html', {})
